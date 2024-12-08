3c164e8403fc7a4a1711915aa689f25ed666a767
Ralph <my@ralphsun.me>
2024-12-09 07:30:31 +0800

    git init

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	import './style/01.scss'
+</script>
+
+<main>
+	<div class="card-title">
+		<h1>九九乘法表</h1>
+		<h2>MULTIPLICATION CHART</h2>
+	</div>
+	<div class="card"></div>
+	<div class="card"></div>
+	<div class="card"></div>
+	<div class="card"></div>
+	<div class="card"></div>
+	<div class="card"></div>
+	<div class="card"></div>
+	<div class="card"></div>
+	<footer>
+		<p>Copyright © 2019 HexSchool. All rights reserved.</p>
+	</footer>
+</main>
