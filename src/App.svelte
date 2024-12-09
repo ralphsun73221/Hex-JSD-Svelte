88584a00a5dc28a720c5b53cec442728eac53492
Ralph <my@ralphsun.me>
2024-12-09 13:11:09 +0800

    Changed: css, README

--- src/App.svelte
+++ src/App.svelte
@@ -8,21 +8,23 @@
 		<h1>九九乘法表</h1>
 		<h2>MULTIPLICATION CHART</h2>
 	</div>
-	<div class="card">
-		<p class="card-title">2</p>
+	<div class="card-bcg">
 		<div class="card-body">
-			{#each arr as arr, i}
-				<p>2 X {arr} = {2 * arr}</p>
-			{/each}
+			<p class="card-title">2</p>
+				{#each arr as arr, i}
+					<p class="card-content">2 X {arr} = {2 * arr}</p>
+				{/each}
+			<!-- <div class="card-content">
+			</div> -->
 		</div>
 	</div>
-	<div class="card"></div>
-	<div class="card"></div>
-	<div class="card"></div>
-	<div class="card"></div>
-	<div class="card"></div>
-	<div class="card"></div>
-	<div class="card"></div>
+	<div class="card-bcg"></div>
+	<div class="card-bcg"></div>
+	<div class="card-bcg"></div>
+	<div class="card-bcg"></div>
+	<div class="card-bcg"></div>
+	<div class="card-bcg"></div>
+	<div class="card-bcg"></div>
 </main>
 <footer>
 	<p>Copyright © 2019 HexSchool. All rights reserved.</p>
