cc4560de29b48253549a5532916c354ee4d9c395
Ralph <my@ralphsun.me>
2024-12-10 07:22:10 +0800

    Changed: 完成第一張卡片裝飾 README

--- src/App.svelte
+++ src/App.svelte
@@ -6,16 +6,28 @@
 
 	// 將 card-content 裡面會使用到的資料統一用函式處理
 	const CARD_CONTENT = (num: number, arr: number[]) => {
-		console.log('目前的 num:', num); // 會看到是 2,3,4...9
-    console.log('完整的 arr:', arr); // 會看到是 [1,2,3,4,5,6,7,8,9]
+		console.log("目前的 num:", num); // 會看到是 2,3,4...9
+		console.log("完整的 arr:", arr); // 會看到是 [1,2,3,4,5,6,7,8,9]
 		return arr.map((item) => `${num} X ${item} = ${num * item}`);
 	};
 </script>
 
 <main>
 	<div class="title">
-		<h1>九九乘法表</h1>
-		<h2>MULTIPLICATION CHART</h2>
+		<div class="line">
+			<span class="corner x-left">x</span>
+			<span class="line-content"></span>
+			<span class="corner x-left">x</span>
+		</div>
+		<div class="title-content">
+			<h1>九九乘法表</h1>
+			<h2>MULTIPLICATION CHART</h2>
+		</div>
+		<div class="line">
+			<span class="corner x-left">x</span>
+			<span class="line-content"></span>
+			<span class="corner x-left">x</span>
+		</div>
 	</div>
 	{#each arr.slice(1) as num}
 		<div class="card-bcg">
@@ -29,5 +41,5 @@
 	{/each}
 </main>
 <footer>
-	<p>Copyright © 2019 HexSchool. All rights reserved.</p>
+	<p>Copyright © 2024 HexSchool. All rights reserved.</p>
 </footer>
