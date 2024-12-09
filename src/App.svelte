7c7679c2cf01b66ebbd2f88e3057ffa7c97b47a4
Ralph <my@ralphsun.me>
2024-12-09 17:17:45 +0800

    Changed: README, 第一版完成

--- src/App.svelte
+++ src/App.svelte
@@ -14,17 +14,64 @@
 				{#each arr as arr, i}
 					<p class="card-content">2 X {arr} = {2 * arr}</p>
 				{/each}
-			<!-- <div class="card-content">
-			</div> -->
 		</div>
 	</div>
-	<div class="card-bcg"></div>
-	<div class="card-bcg"></div>
-	<div class="card-bcg"></div>
-	<div class="card-bcg"></div>
-	<div class="card-bcg"></div>
-	<div class="card-bcg"></div>
-	<div class="card-bcg"></div>
+	<div class="card-bcg">
+		<div class="card-body">
+			<p class="card-title">3</p>
+				{#each arr as arr, i}
+					<p class="card-content">3 X {arr} = {3 * arr}</p>
+				{/each}
+		</div>
+	</div>
+	<div class="card-bcg">
+		<div class="card-body">
+			<p class="card-title">4</p>
+				{#each arr as arr, i}
+					<p class="card-content">4 X {arr} = {4 * arr}</p>
+				{/each}
+		</div>
+	</div>
+	<div class="card-bcg">
+		<div class="card-body">
+			<p class="card-title">5</p>
+				{#each arr as arr, i}
+					<p class="card-content">5 X {arr} = {5 * arr}</p>
+				{/each}
+		</div>
+	</div>
+	<div class="card-bcg">
+		<div class="card-body">
+			<p class="card-title">6</p>
+				{#each arr as arr, i}
+					<p class="card-content">6 X {arr} = {6 * arr}</p>
+				{/each}
+		</div>
+	</div>
+	<div class="card-bcg">
+		<div class="card-body">
+			<p class="card-title">7</p>
+				{#each arr as arr, i}
+					<p class="card-content">7 X {arr} = {7 * arr}</p>
+				{/each}
+		</div>
+	</div>
+	<div class="card-bcg">
+		<div class="card-body">
+			<p class="card-title">8</p>
+				{#each arr as arr, i}
+					<p class="card-content">8 X {arr} = {8 * arr}</p>
+				{/each}
+		</div>
+	</div>
+	<div class="card-bcg">
+		<div class="card-body">
+			<p class="card-title">9</p>
+				{#each arr as arr, i}
+					<p class="card-content">9 X {arr} = {9 * arr}</p>
+				{/each}
+		</div>
+	</div>
 </main>
 <footer>
 	<p>Copyright © 2019 HexSchool. All rights reserved.</p>
