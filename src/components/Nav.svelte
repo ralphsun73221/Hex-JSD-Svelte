a84ef275c9ef36b648ced2d8c53e011c327facd8
Ralph <my@ralphsun.me>
2024-12-10 20:46:30 +0800

    Changed: index.html 多餘的程式碼

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -3,8 +3,8 @@
 
 <nav>
 	<ul>
-		<li><a href="./src/pages/01.html">九九乘法表</a></li>
-		<li><a href="./src/pages/01.html">時鐘</a></li>
+		<li><a href="/">九九乘法表</a></li>
+		<li><a href="./src/pages/02.html">時鐘</a></li>
 	</ul>
 </nav>
 
