2bbb84397b855da97cb25f34a26dac5f670cc380
Ralph <my@ralphsun.me>
2024-12-13 11:21:25 +0800

    Changed: README, Added: Clock

---
+++ src/components/Clock.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Nav from './Nav.svelte';
+	
+</script>
+
+<Nav />
+
+<style lang="scss">
+	
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { mount } from 'svelte'
 import './reset.scss'
-// import Main from './components/Main.svelte'
 import Chengfabiao from './components/Chengfabiao.svelte'
 
 const app = mount(Chengfabiao, {
