2560834d2c5068f9f9820a4f55f63cece34f8e22
Ralph <my@ralphsun.me>
2024-12-13 12:17:47 +0800

    Added: assets/clock-bg

--- src/components/Clock.svelte
+++ src/components/Clock.svelte
@@ -5,21 +5,41 @@
 <Nav />
 <main>
 	<div class="clock-bg">
+		<div class="clock"></div>
 	</div>
 </main>
 
 <style lang="scss">
 	$main-color: #293b29;
 	$senderay-color: #435947;
-	$hour: #EE7E31;
+	$hour: #ee7e31;
 	$minute: #ffffff;
-	$seconds: #C1FD50;
+	$seconds: #c1fd50;
 
 	main {
 		width: 1200px;
 		height: 800px;
 		margin: 0 auto;
+		align-content: center;
 		background-color: $main-color;
 		color: $minute;
+
+		.clock-bg {
+			background-image: url("/src/assets/clock-bg.svg");
+			width: 500px;
+			height: 500px;
+			margin: 0 auto;
+			/* align-content: center; */
+			/* background-color: $senderay-color;
+			border-radius: 25%; */
+
+			.clock {
+				/* width: 300px;
+				height: 300px;
+				margin: 0 auto;
+				background-color: $main-color;
+				border-radius: 100%; */
+			}
+		}
 	}
 </style>
