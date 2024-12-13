5d6de26fd6558663c95008ae8631587094794507
Ralph <my@ralphsun.me>
2024-12-13 11:40:22 +0800

    Added: SCSS, HTML

--- src/components/Clock.svelte
+++ src/components/Clock.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
-  import Nav from './Nav.svelte';
-	
+	import Nav from "./Nav.svelte";
 </script>
 
 <Nav />
+<main>
+	<div class="clock-bg">
+	</div>
+</main>
 
 <style lang="scss">
-	
+	$main-color: #293b29;
+	$senderay-color: #435947;
+	$hour: #EE7E31;
+	$minute: #ffffff;
+	$seconds: #C1FD50;
+
+	main {
+		width: 1200px;
+		height: 800px;
+		margin: 0 auto;
+		background-color: $main-color;
+		color: $minute;
+	}
 </style>
